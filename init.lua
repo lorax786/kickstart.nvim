7a184fdf6037de213558649cbe1731751e7f33ad
Ali Malik <ali.m.malik@gmail.com>
2025-02-26 15:53:36 -0500

    added typescript lsp

--- init.lua
+++ init.lua
@@ -685,7 +685,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        ts_ls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
